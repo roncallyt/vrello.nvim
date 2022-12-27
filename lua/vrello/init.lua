b3e8415231187800c471ea6b83a50f14d3c24510
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2022-12-27 00:45:02 -0400

    Add read function

--- lua/vrello/init.lua
+++ lua/vrello/init.lua
@@ -3,14 +3,9 @@ local utils = require("vrello.utils")
 local Dev = require("vrello.dev")
 local log = Dev.log
 
-local config_path = vim.fn.stdpath("config")
 local config = string.format("%s/vrello.json", utils.project_path())
 
 local M = {}
-
-function M.get_config_path()
-  print(config)
-end
 
 local vrello_augroup = vim.api.nvim_create_augroup("VRELLO_AUGROUP", {
   clear = true
@@ -52,10 +47,17 @@ local function merge_tables(...)
     return out
 end
 
+local function read_config(config)
+    log.trace("_read_config():", config)
+    return vim.json.decode(Path:new(config):read())
+end
+
 -- 1. saved.  Where do we save?
 function M.setup(config)
-    log.debug("setup(): Complete config", HarpoonConfig)
-    log.trace("setup(): log_key", Dev.get_log_key())
+  print(read_config())
+
+  log.debug("setup(): Complete config", VrelloConfig)
+  log.trace("setup(): log_key", Dev.get_log_key())
 end
 
 -- Sets a default config with no values
