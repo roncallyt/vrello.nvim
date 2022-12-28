fbc2b8a46a26cac88cefbf9fe0893711db2a9569
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2022-12-27 21:06:46 -0400

    Add function to test curl

--- lua/vrello/init.lua
+++ lua/vrello/init.lua
@@ -135,7 +135,7 @@ function M.print_config()
 end
 
 function M.test_curl()
-  local url = "http://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
+  local url = "https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow"
 
   Job:new({
     command = "curl",
