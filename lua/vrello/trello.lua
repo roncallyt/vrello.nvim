53df748176b1611ae2152957c9237c4be5d4933d
Thomerson Roncally Araújo Teixeira <thomersonroncally@outlook.com>
2022-12-28 09:12:03 -0400

    Add function to get cards from Trello API

--- lua/vrello/trello.lua
+++ lua/vrello/trello.lua
@@ -1,5 +1,6 @@
 local Path = require("plenary.path")
 local Job = require("plenary.job")
+local log = require("vrello.dev").log
 local utils = require("vrello.utils")
 
 local M = {}
@@ -17,7 +18,28 @@ function M.get_boards()
     "'Accept: application/json'"
   }, vim.loop.cwd())
 
-  print(vim.inspect(vim.fn.json_decode(out)))
+  return vim.fn.json_decode(out)
+end
+
+function M.get_cards_by_query()
+  if VrelloConfig.board == "" then
+    log.debug("get_cards(): No board config found for:", utils.project_path())
+    return
+  end
+
+  local url = "https://api.trello.com/1/search?idBoards=" .. VrelloConfig.board .. "&query=" .. VrelloConfig.query .. "&card_fields=name,due,desc,url&key=" .. VrelloConfig.key .. "&token=" .. VrelloConfig.token
+
+  local out, ret, _ = utils.get_os_command_output({
+    "curl",
+    "--request",
+    "GET",
+    "--url", 
+    url, 
+    "--header", 
+    "'Accept: application/json'"
+  }, vim.loop.cwd())
+
+  return vim.fn.json_decode(out)
 end
 
 return M
